130eb34c9b9233af8620aea6cfe28816c4d5b079
ecf17 <74380A@nur.edu.bo>
2023-04-02 16:00:42 -0400

    Modificado constructor para utilizar modelo y vista, creado metodos para controlar la vista

--- src/main/java/org/p1/Controlador.java
+++ src/main/java/org/p1/Controlador.java
@@ -1,10 +1,28 @@
 package org.p1;
 
-public class Controlador {
 
-    public void iniciar(){
-        Vista vista = new Vista();
-        vista.setVisible(true);
+public class Controlador {
+    private Modelo modelo;
+    private Vista vista;
+    public Controlador(Modelo m, Vista v){
+        modelo = m;
+        vista = v;
     }
 
+    //Metodos para controlar menus
+    public void salir(){
+        System.exit(0);
+    }
+    public void arrAgregarElemento(){
+        modelo.agregarElemento(10);
+    }
+    public void arrEliminarElemento(){
+        modelo.setElemento(-1, 0);
+        System.out.println("Se ha eliminado");
+    }
+
+    public void iniciar(){
+        Vista vista = new Vista(modelo);
+        vista.setVisible(true);
+    }
 }
