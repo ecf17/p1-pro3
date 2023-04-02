379378e093b164dd12cdc26324919440cf9f4a29
ecf17 <74380A@nur.edu.bo>
2023-04-02 16:28:48 -0400

    Test metodo reset de controlador

--- src/test/java/ControladorArreglo.java
+++ src/test/java/ControladorArreglo.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class ControladorArreglo {
+import org.junit.jupiter.api.Test;
+import org.p1.Controlador;
+import org.p1.Modelo;
+import org.p1.Vista;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ControladorArreglo {
+
+    @Test
+    void ResetDeberiaDarArregloDeCeros(){
+        int[] arrayVacio = new int[100];
+        Modelo modelo = new Modelo();
+        Vista vista = new Vista(modelo);
+        modelo.agregarElemento(1);
+        modelo.agregarElemento(2);
+        System.out.println(modelo.getElemento(0));
+        System.out.println(modelo.getElemento(1));
+        Controlador controlador = new Controlador(modelo, vista);
+        controlador.reset();
+        int[] resultado = modelo.getArreglo();
+
+        assertArrayEquals(arrayVacio, resultado);
+    }
 }
