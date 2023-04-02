34fc3258db05e381ec3a185f383785627ab0bb82
ecf17 <74380A@nur.edu.bo>
2023-04-02 16:02:54 -0400

    Creado metodo agregar elemento al array y creado metodo para eliminar ultimo elemento

--- src/main/java/org/p1/Modelo.java
+++ src/main/java/org/p1/Modelo.java
@@ -12,13 +12,29 @@ public class Modelo {
     public int getElemento(int posicion){
         return arreglo[posicion];
     }
-
     public void setArreglo(int[] arreglo) {
         this.arreglo = arreglo;
     }
 
     public void setElemento(int posicion, int valor){
         arreglo[posicion] = valor;
+    }
+    public void agregarElemento(int valor) throws ArrayIndexOutOfBoundsException{
+        boolean fueAgregado = false;
+        for (int i = 0; i < arreglo.length; i++) {
+            if(arreglo[i] == 0){
+                arreglo[i] = valor;
+                fueAgregado = true;
+                break;
+            }
+        }
+        if(!fueAgregado){
+            throw new ArrayIndexOutOfBoundsException("El arreglo está lleno");
+        }
+    }
+
+    public void eliminarUltimoElemento(){
+        arreglo[arreglo.length-1] = 0;
     }
 
 
