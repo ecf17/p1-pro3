66b6a5fe8ed5ab07f8d84f35f30e3fd8683140bc
ecf17 <74380A@nur.edu.bo>
2023-04-02 12:08:50 -0400

    creado barra de menu con items correspondientes

--- src/main/java/org/p1/Vista.java
+++ src/main/java/org/p1/Vista.java
@@ -4,10 +4,41 @@ import javax.swing.*;
 
 public class Vista extends JFrame {
 
+    private JMenuBar menuBar;
+    private JMenu menuArchivo, menuArreglo;
+    private JMenuItem itemReset, itemSalir, itemArrAgregar, itemArrEliminar, itemArrAzar, itemArrOrdenar;
 
     public Vista(){
+        //Config frame
         setSize(600, 400);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        //Creación barra menu
+        menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        //Creación menu archivo arreglo y añadido al menuBar
+        menuArchivo = new JMenu("Archivo");
+        menuArreglo = new JMenu("Arreglo");
+        menuBar.add(menuArchivo);
+        menuBar.add(menuArreglo);
+
+        //Crear y añadir items a menu archivo
+        itemReset = new JMenuItem("Reset");
+        itemSalir = new JMenuItem("Salir");
+        menuArchivo.add(itemReset);
+        menuArchivo.add(itemSalir);
+
+        //Crear y añadir items a menu arreglo
+        itemArrAgregar = new JMenuItem("Agregar");
+        itemArrEliminar = new JMenuItem("Eliminar");
+        itemArrAzar = new JMenuItem("Azar");
+        itemArrOrdenar = new JMenuItem("Ordenar");
+        menuArreglo.add(itemArrAgregar);
+        menuArreglo.add(itemArrEliminar);
+        menuArreglo.add(itemArrAzar);
+        menuArreglo.add(itemArrOrdenar);
     }
 }
