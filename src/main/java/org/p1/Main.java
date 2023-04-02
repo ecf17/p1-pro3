e69e6f8a6b739cdf7be37c7d7ee110048282687b
ecf17 <74380A@nur.edu.bo>
2023-04-02 18:49:09 -0400

    .

--- src/main/java/org/p1/Main.java
+++ src/main/java/org/p1/Main.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static Logger logger = LogManager.getRootLogger();
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
 
